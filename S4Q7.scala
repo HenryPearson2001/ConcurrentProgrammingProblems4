628f522945ce62724c1fab8dc954453c1a5596c2
Henry Pearson <pearsonhenryj@icloud.com>
2021-03-02 21:51:32 +0000

    added in the test

--- S4Q7.scala
+++ S4Q7.scala
@@ -30,11 +30,11 @@ class pairSemaphore {
         }
     }
 
-    def womanSync(me: String): String = monitor.withLock {
+    def womanSync(me: String): String = {
         mutex.down
         if (!men.isEmpty) {
             val (sem, id) = men.dequeue
-            pairs = (me, id)::pairs
+            pairs = (id, me)::pairs
             sem.up
             id
         }
@@ -43,7 +43,7 @@ class pairSemaphore {
             women.enqueue((mySem, me))
             mutex.up
             mySem.down
-            val (_, id) = pairs(pairs.indexWhere(_._1 == me))
+            val (id, _) = pairs(pairs.indexWhere(_._2 == me))
             mutex.up
             id
         }
@@ -51,3 +51,71 @@ class pairSemaphore {
 
 }
 
+import scala.util.Random
+
+object pairSemaphoreTest {
+    // N is number of clients, time is time server is run for
+    private val N = 5; private val time = 1
+
+    private var pairs = List[pair]()
+
+    private var myPairSemaphore = new pairSemaphore
+
+    // to communicate between clients and test - for simplicity women are first string
+    private type pair = (String, String)
+    val pairChan = ManyOne[pair]
+
+    def receivePairs = proc {
+        for (i <- 0 until N + N) {
+            // println(i)
+            pairs = pairChan?()::pairs
+        }
+    }
+
+    // process to simulate a client - will request a pair then send the pairing to the test method
+    // true for women, false for men
+    def client(me: String, sex: Boolean) = proc {
+        attempt {
+            if (sex) {
+                val result = myPairSemaphore.manSync(me)
+                pairChan!(result, me)
+            }
+            else {
+                val result = myPairSemaphore.womanSync(me)
+                pairChan!(me, result)
+            }
+        }
+        {}
+    }
+
+    // checks if every possible pair has been formed (either only men or only women without a pair)
+    // and checks each pair has exactly one man and one woman
+    def checkPairs(men: Array[String], women: Array[String]): Boolean = {
+        var womenUnpaired = false
+        var menUnpaired = false
+        for ((man, woman) <- pairs) {
+            if (man == "N/A") womenUnpaired = true
+            if (woman == "N/A") menUnpaired = true
+            if ((!men.contains(man)) && (man != "N/A")) false
+            if ((!women.contains(woman)) && (woman != "N/A")) false
+        }
+        if (menUnpaired && womenUnpaired) false
+        true
+    }
+
+    def doTest = {
+        val numberOfMen = Random.nextInt(N)
+        val men = Array.fill(N)(Random.alphanumeric.take(10).mkString)
+        val women = Array.fill(N)(Random.alphanumeric.take(10).mkString)
+        val clients = (|| (for (person <- men) yield client(person, false))) || (|| (for (person <- women) yield client(person, true)))
+        run (clients || receivePairs)
+        assert(checkPairs(men, women))
+    }
+
+    def main(args: Array[String]) = {
+        for(i <- 0 until 1000) {
+            doTest; if(i%10 == 0) print(".")
+        }
+        println; io.threadcso.exit
+    }
+}

d4be1e83f45c99b4ec0cc1b58a546e6a3f01df34
Henry Pearson <pearsonhenryj@icloud.com>
2021-03-02 20:47:20 +0000

    implemented test

--- S4Q5.scala
+++ S4Q5.scala
@@ -38,3 +38,53 @@ class resource {
         else mutex.up
     }
 }
+import ox.cads.testing._
+import scala.collection.immutable.Set
+
+object resourceTest {
+
+    type SeqDatabase = Set[Int]
+    type ConcDatabase = resource
+
+    // number of readers
+    val N = 5
+    // number of iterations
+    val I = 10
+
+    // functions for sequential implementation
+    // precondition that the sum of ids must be a multiple of 3
+    def seqEnter(me: Int)(set: Set[Int]) : (Unit, Set[Int]) = {
+        require(set.sum % 3 == 0)
+        ((), set.incl(me))
+    }
+
+    def seqExit(me: Int)(set: Set[Int]) : (Unit, Set[Int]) = {
+        ((), set.excl(me))
+    }
+
+    def reader(me: Int, log: GenericThreadLog[SeqDatabase, ConcDatabase]) = {
+        for (i <- 0 until I) {
+            // attempt to access the resource
+            log.log(_.enter(me), "Entering", seqEnter(me))
+            // attempt to exit accessing the resource
+            log.log(_.exit(me), "Exiting", seqExit(me))
+        }
+    }
+
+    def doTest = {
+        // concurrent resource monitor object
+        val concDatabase = new resource
+        // set to store which processes are active for sequential part of test
+        val seqDatabase: Set[Int] = Set()
+        val tester = LinearizabilityTester.JITGraph[SeqDatabase, ConcDatabase](seqDatabase, concDatabase, N, reader _, 2 * I)
+        assert(tester() > 0)
+    }
+
+    def main(args: Array[String]) = {
+        for(i <- 0 until 1000){
+            doTest
+            if(i%10 == 0) print(".")
+        }
+        println; io.threadcso.exit
+    }
+}
